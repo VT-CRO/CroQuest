b371fd76d07eda69cb84aefd10af19cf8caee42f
ConnorMcCue23 <149333976+ConnorMcCue23@users.noreply.github.com>
2025-03-30 10:40:05 -0400

    Add files via upload

---
+++ snake.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <unistd.h>
+
+// Creating the board width and height
+// Easy to change once we have sizes confirmed
+#define BOARD_WIDTH = 20;
+#define BOARD_HEIGHT = 20;
+
+// Directional functionality
+// Don't know how much this will change, can be a lot
+#define
