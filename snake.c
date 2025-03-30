64188ea24ce2b960d348e1ae5c52bd8458f27ecd
ConnorMcCue23 <connormccue23@vt.edu>
2025-03-30 11:08:40 -0400

    functionality for direction and game initialization

--- snake.c
+++ snake.c
@@ -10,4 +10,35 @@
 
 // Directional functionality
 // Don't know how much this will change, can be a lot
-#define
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
+typedef struct
+{
+    int x;
+    int y;
+} Location;
+
+// How big the snake can grow?
+Location snake[100];
+
+// Starting off with a length of 1
+int s_length = 1;
+
+// Starting with the direction moving right
+int dir = RIGHT;
+
+// Food
+Location food;
+
+// Creating the game
+void init_snake()
+{
+    // Snake starting location
+    snake[0].x = BOARD_WIDTH / 2;
+    snake[0].x = BOARD_HEIGHT / 2;
+    food.x = rand() % BOARD_WIDTH;
+    food.y = rand() % BOARD_HEIGHT;
+}
