f3b0fbc194fa613bda5c271d5e89218f480eb252
ConnorMcCue23 <connormccue23@vt.edu>
2025-04-05 18:07:18 -0400

    working on adding the assets to the code and finally fixing tkinter

--- snake.py
+++ snake.py
@@ -1,28 +1,36 @@
 import tkinter as tk
+import os
 from PIL import Image, ImageTk
 
 class Snake(tk.Canvas):
-    def __init__(self):
-        super().__init__(width = 20, height = 20, background = "black", highlightthickness=0)
+    def __init__(self, parent):
+        super().__init__(width = 600, height = 600, background="black", highlightthickness=0)
+
+        self.snake_positions = [(100, 100), (80, 100), (60, 100)]
 
         self.load_assets()
+        self.create_objects()
     
     def load_assets(self):
         try:
-            self.snake_body_png = Image.open("./assets/snake.png")
+            self.snake_body_png = Image.open("./assets/body_segment.png")
             self.snake_body = ImageTk.PhotoImage(self.snake_body_png)
 
-            self.apple_image_png = Image.open("./assets/apple.png")
+            self.apple_image_png = Image.open("./assets/Apple.png")
             self.apple = ImageTk.PhotoImage(self.apple_image_png)
         except IOError as error:
             print(error)
             root.destroy()
 
+    def create_objects(self):
+        for x_position, y_position in self.snake_positions:
+            self.create_image (x_position, y_position, image=self.snake_body, tag="snake")
+
 root = tk.Tk()
 root.title("CroQuest Snake")
 root.resizable(False, False)
 
-board = Snake()
+board = Snake(root)
 board.pack()
 
 root.mainloop()
