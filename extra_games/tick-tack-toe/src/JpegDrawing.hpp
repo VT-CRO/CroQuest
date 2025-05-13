f1f3ab18501c5d11a99cda7e151b48d5f3f8d2f2
lshad03 <lshad03@vt.edu>
2025-05-13 12:33:52 -0400

    tic-tac-toe assets added

---
+++ extra_games/tick-tack-toe/src/JpegDrawing.hpp
@@ -0,0 +1,40 @@
+#ifndef JPEG_DRAWING_HPP
+#define JPEG_DRAWING_HPP
+
+#include <TFT_eSPI.h>
+#include <JPEGDecoder.h>
+#include <SPI.h>
+#include <SD.h>
+
+class JpegDrawing {
+private:
+    TFT_eSprite sprite;
+    bool swapBytes;
+    bool buffer_created;
+    int buffer_width;
+    int buffer_height;
+    int x_pos;
+    int y_pos;
+    bool first = true;
+    
+    bool createBuffer(int width, int height);
+    void jpegRender(int xpos, int ypos);
+    
+public:
+    // Constructor
+    JpegDrawing(TFT_eSPI& tft);
+    
+    // Public struct for image information
+    struct ImageInfo {
+        uint32_t width;
+        uint32_t height;
+        bool valid;
+    };
+    
+    // Public methods
+    void drawSdJpeg(const char *filename, int xpos, int ypos);
+    void pushSprite();
+    ImageInfo getJpegDimensions(const char *filename);
+};
+
+#endif // JPEG_DRAWING_HPP
