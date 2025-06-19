4e0bed63256af6ebe5ad4f524a6dea31641129b1
houndsito <fcampoverdeg@vt.edu>
2025-06-19 09:48:49 -0400

    added .pio

---
+++ embedded_code/ESP32/.pio/build/esp32dev/src/Core/BrightnessControl.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/src/Core/BrightnessControl.cpp.o: \
+ src/Core/BrightnessControl.cpp
---
+++ embedded_code/ESP32/.pio/build/esp32dev/src/SettingsMenu/BrightnessMenu/Brightness.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/src/SettingsMenu/BrightnessMenu/Brightness.cpp.o: \
+ src/SettingsMenu/BrightnessMenu/Brightness.cpp
