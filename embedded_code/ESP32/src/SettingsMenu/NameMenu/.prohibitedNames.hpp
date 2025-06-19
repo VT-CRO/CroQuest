e550661a769e769e1ecdf096db1766780e0f4ff1
houndsito <fcampoverdeg@vt.edu>
2025-06-19 09:44:02 -0400

    Prohibited names

---
+++ embedded_code/ESP32/src/SettingsMenu/NameMenu/.prohibitedNames.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+static const std::vector<std::string> prohibitedNames = {
+    "ASSXX", "PUSXX",  "FUCKX",  "SHITX", "COCKX",  "DICXX", "CUNTX", "BITCHX",
+    "NOOXX", "FAGGX",  "NIGXX",  "CUMXX", "PENISX", "TITTX", "FARTX", "CRAPX",
+    "WANKX", "TWATX",  "BOLLOX", "SUCKX", "BLOWX",  "PISSX", "IDIXX", "WHOXX",
+    "WICKX", "RAPXXX", "SExXX ", "DUMBX", "BOOBX"};
